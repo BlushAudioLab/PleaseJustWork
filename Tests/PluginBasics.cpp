9aa5b6347a07bb568d08f20d083ff5927dd7ae09
Blush Audio <65124538+BlushAudioLab@users.noreply.github.com>
2022-11-30 18:30:57 +0000

    Update PluginBasics.cpp

--- Tests/PluginBasics.cpp
+++ Tests/PluginBasics.cpp
@@ -13,5 +13,5 @@ EggMachineAudioProcessor testPlugin;
 TEST_CASE("Plugin instance name", "[name]")
 {
   CHECK_THAT(testPlugin.getName().toStdString(),
-             Catch::Matchers::Equals("Pamplejuce"));
+             Catch::Matchers::Equals("EggMachine"));
 }
